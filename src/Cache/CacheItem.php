e04147670ffbaa13c777bbc33ce6769560c2d1e1
martinyde <yde001@gmail.com>
2025-07-04 11:07:44 +0200

    Updated dependencies

--- src/Cache/CacheItem.php
+++ src/Cache/CacheItem.php
@@ -30,6 +30,13 @@ class CacheItem implements CacheItemInterface {
   private bool $isHit;
 
   /**
+   * The expiry.
+   *
+   * @var float|null
+   */
+  private ?float $expiry;
+
+  /**
    * CacheItem constructor.
    *
    * @param string $key
